738db9956d63db189cbc8b34fc8b8121903b94f0
dgardner88 <david.gardner@students.olin.edu>
2011-02-04 20:31:40 -0500

    trying again

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Youknowolin::Application.config.secret_token = 'cd1ea4b64e0bf0d34de6be2b9c8237e35c22ccfdab3dd49befeb7db55153c3c62e131d0991cdbbbd94cf55e13d88ade963334aaa90b1727134ca1f9dd27ffa67'
